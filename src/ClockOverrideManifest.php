9eef2d219daebc2409dcf42c3e25eda89cb26d7a
Tomáš Chochola <tomaschochola@tomaschochola.cz>
2026-03-08 04:32:42 +0100

    Changes of the day 2026-03-08T04:32:42+01:00

---
+++ src/ClockOverrideManifest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @author Tomáš Chochola <tomaschochola@tomaschochola.cz>
+ * @copyright © 2026 Tomáš Chochola <tomaschochola@tomaschochola.cz>
+ *
+ * @license CC-BY-ND-4.0
+ *
+ * @see {@link https://creativecommons.org/licenses/by-nd/4.0/} License
+ * @see {@link https://github.com/tomaschochola} GitHub Profile
+ * @see {@link https://github.com/sponsors/tomaschochola} GitHub Sponsors
+ */
+
+declare(strict_types=1);
+
+namespace TomasChochola\Psr\Clock;
+
+use IteratorAggregate;
+use NoDiscard;
+use Override;
+use Psr\Clock\ClockInterface;
+use Traversable;
+
+/**
+ * @no-named-arguments
+ *
+ * @implements IteratorAggregate<mixed, mixed>
+ */
+readonly class ClockOverrideManifest implements IteratorAggregate
+{
+    #[NoDiscard]
+    #[Override]
+    public function getIterator(): Traversable
+    {
+        yield FixedClock::class => [FixedClock::class, 'unload'];
+
+        yield ClockInterface::class => [FixedClock::class, 'unload'];
+    }
+}
